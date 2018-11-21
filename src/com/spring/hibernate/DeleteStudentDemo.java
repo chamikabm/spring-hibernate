a84e376b3b5e48a20ecaff049f5e3ccf343bfafb
chamikab <kasun.chamika@gmail.com>
2018-11-22 00:52:18 +0530

    Added changes for deleting entries from mysql database with hibernate.

---
+++ src/com/spring/hibernate/DeleteStudentDemo.java
@@ -0,0 +1,26 @@
+package com.spring.hibernate;
+
+import com.spring.hibernate.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteStudentDemo {
+    public static void main(String[] args) {
+
+        try(SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Student.class)
+        .buildSessionFactory()) {
+
+            Session session = sessionFactory.getCurrentSession();
+
+            session.beginTransaction();
+            int studentId = 2;
+            Student student = session.get(Student.class, studentId);
+            session.delete(student);
+
+            session.createQuery("delete from Student where id=4").executeUpdate();
+            session.getTransaction().commit();
+        }
+    }
+}
--- src/com/spring/hibernate/UpdateStudentDemo.java
+++ src/com/spring/hibernate/UpdateStudentDemo.java
@@ -1,5 +1,6 @@
-package com.spring.hibernate.entity;
+package com.spring.hibernate;
 
+import com.spring.hibernate.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
