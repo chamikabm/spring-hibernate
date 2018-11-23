a4242ce02b4533ea3eb5cd20ccf161e4e349c20d
chamikab <kasun.chamika@gmail.com>
2018-11-23 22:32:52 +0530

    Added changes for mapping many to many.

---
+++ src/com/spring/hibernate/many_to_many/entity/Course.java
@@ -0,0 +1,97 @@
+package com.spring.hibernate.many_to_many.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH
+    })
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    @ManyToOne(
+            fetch =FetchType.LAZY,
+            cascade = {
+                    CascadeType.DETACH, CascadeType.MERGE,
+                    CascadeType.PERSIST, CascadeType.REFRESH
+            }
+     )
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
+    public Course() {
+        // This is a default no arg constructor created as it is required by hibernate.
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    // Add convenience methods for adding a student
+    public List<Student> addStudent(Student student) {
+        if (students == null ) {
+            students = new ArrayList<>();
+        }
+
+        students.add(student);
+        return  students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
