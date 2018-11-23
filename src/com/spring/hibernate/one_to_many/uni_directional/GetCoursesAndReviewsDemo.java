213c0e7ae3bce33eaa1ff57428edf2e065c6c8df
chamikab <kasun.chamika@gmail.com>
2018-11-23 11:25:28 +0530

    Added changes for unidirectional one to many mappings deleted.

---
+++ src/com/spring/hibernate/one_to_many/uni_directional/GetCoursesAndReviewsDemo.java
@@ -0,0 +1,39 @@
+package com.spring.hibernate.one_to_many.uni_directional;
+
+import com.spring.hibernate.one_to_many.uni_directional.entity.Course;
+import com.spring.hibernate.one_to_many.uni_directional.entity.Instructor;
+import com.spring.hibernate.one_to_many.uni_directional.entity.InstructorDetail;
+import com.spring.hibernate.one_to_many.uni_directional.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCoursesAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        try(SessionFactory sessionFactory = new Configuration()
+                .configure("com/spring/hibernate/one_to_many/hibernate.cfg.xml")
+            .addAnnotatedClass(Instructor.class)
+            .addAnnotatedClass(InstructorDetail.class)
+            .addAnnotatedClass(Course.class)
+            .addAnnotatedClass(Review.class)
+            .buildSessionFactory()
+        ) {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            // Fetch a course.
+            int courseId = 10;
+            Course course = session.get(Course.class, courseId);
+
+            // Print course
+            System.out.println(course);
+
+            // Print reviews.
+            System.out.println(course.getReviews());
+
+            session.getTransaction().commit();
+        }
+    }
+}
